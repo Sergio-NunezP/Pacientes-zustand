eea76993fb01bacdb6a1c8b07b8ddece0138d3e3
Sergio Núñez <sergioop722@gmail.com>
2025-02-19 07:40:43 -0500

    Se muestra Toast en editar y eliminar

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,6 +1,8 @@
+import { toast } from 'react-toastify'
 import { Patient } from "../types"
 import PatientDetailItem from "./PatientDetailItem"
 import { usePatientStore } from "../store"
+
 
 export type PatientDetailsProps = {
     patient: Patient
@@ -10,7 +12,10 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
 
     const deletePatient = usePatientStore((state) => state.deletePatient)
     const getPatientById = usePatientStore((state) => state.getPatientById)
-
+    const handleClick = () => {
+        deletePatient(patient.id)
+        toast.error('Paciente Eliminado')
+    }
 
     return (
         <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
@@ -31,7 +36,7 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
                 <button
                     type="button"
                     className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-                    onClick={() => deletePatient(patient.id)}
+                    onClick={handleClick}
                 >Eliminar</button>
 
             </div>
